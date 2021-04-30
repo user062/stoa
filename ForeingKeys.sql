330d7bca20e9c0c359eaf76ba737eb3c0d654b49
abdelfettah-Dz <45067188+abdelfettah-Dz@users.noreply.github.com>
2021-04-30 15:18:34 +0200

    Add files via upload
    data base creation

---
+++ ForeingKeys.sql
@@ -0,0 +1,57 @@
+alter table COMMENTAIRE add constraint FK_COMMENTE foreign key (COMPTEID)
+      references COMPTE (COMPTEID) on delete cascade;
+
+alter table COMMENTAIRE add constraint FK_CONSERNE1 foreign key (ID_REPONSE)
+      references REPONSE (ID_REPONSE) on delete cascade;
+
+alter table CONCERNE add constraint FK_CONCERNE foreign key (ID_DOSSIER)
+      references DOSSIER (ID_DOSSIER) on delete cascade;
+
+alter table CONCERNE add constraint FK_CONCERNE2 foreign key (POST_ID)
+      references POST (POST_ID) on delete cascade;
+
+alter table DOCUMENT add constraint FK_CONSISTE foreign key (ID_DOSSIER)
+      references DOSSIER (ID_DOSSIER) on delete cascade;
+
+alter table DOSSIER add constraint FK_CREE foreign key (COMPTEID)
+      references COMPTE (COMPTEID) on delete cascade;
+
+alter table DOSSIER add constraint FK_FAIT_PARTI foreign key (ID_MODULE)
+      references MODULE (ID_MODULE) on delete cascade;
+
+alter table FICHIER_1 add constraint FK_AJOUTE foreign key (POST_ID)
+      references POST (POST_ID) on delete cascade;
+
+alter table FICHIER_2 add constraint FK_AJOUTE2 foreign key (ID_REPONSE)
+      references REPONSE (ID_REPONSE) on delete cascade;
+
+alter table INSCRET add constraint FK_INSCRET foreign key (COMPTEID)
+      references COMPTE (COMPTEID) on delete cascade;
+
+alter table INSCRET add constraint FK_INSCRET2 foreign key (ID_DOSSIER)
+      references DOSSIER (ID_DOSSIER) on delete cascade;
+
+alter table POLL_ELEMENT add constraint FK_PEUT_AVOIR foreign key (POST_ID)
+      references POST (POST_ID) on delete cascade;
+
+alter table POLL_VOTE add constraint FK_POLL_VOTE foreign key (COMPTEID)
+      references COMPTE (COMPTEID) on delete cascade;
+
+alter table POLL_VOTE add constraint FK_POLL_VOTE2 foreign key (POLL_ID)
+      references POLL_ELEMENT (POLL_ID) on delete cascade;
+
+alter table POST add constraint FK_CREE_POST foreign key (COMPTEID)
+      references COMPTE (COMPTEID) on delete cascade;
+
+alter table REPONSE add constraint FK_AJOUTE_REPONSE foreign key (COMPTEID)
+      references COMPTE (COMPTEID) on delete cascade;
+
+alter table REPONSE add constraint FK_CONSERNE2 foreign key (POST_ID)
+      references POST (POST_ID) on delete cascade;
+
+alter table UP_DOWN_VOTE add constraint FK_UP_DOWN_VOTE foreign key (COMPTEID)
+      references COMPTE (COMPTEID) on delete cascade;
+
+alter table UP_DOWN_VOTE add constraint FK_UP_DOWN_VOTE2 foreign key (ID_REPONSE)
+      references REPONSE (ID_REPONSE) on delete cascade;
+
---
+++ triggers.sql
@@ -0,0 +1,12 @@
+create trigger T1 before insert on COMMENTAIRE
+for each row
+set new.DATE_AJOUTE= CURRENT_DATE;
+create trigger T2 before insert on post
+for each row
+set new.DATE_AJOUTE= CURRENT_DATE;
+create trigger T3 before insert on reponse
+for each row
+set new.DATE_AJOUTE= CURRENT_DATE;
+create trigger T4 before insert on inscret
+for each row
+set new.DATE_INSCIT= CURRENT_DATE;
