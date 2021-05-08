cfcf8411fb7d6de69f2bc23a361fd17ac2986089
abdelfettah-Dz <45067188+abdelfettah-Dz@users.noreply.github.com>
2021-05-08 22:29:45 +0200

    fixed event condition

--- events.sql
+++ events.sql
@@ -6,11 +6,14 @@
 -- ------------------------------------------------------------------------
 -- validation event 1 --
 create event if not exists E1
-on schedule every 1 minute
+on schedule every 1 hour
 starts current_timestamp
 do 
 delete from compte 
 where 
-((vcode <> 0) and (hour(current_timestamp - date_inscrit) > 6));
+vcode <> 0
+and
+ {fn TIMESTAMPDIFF(SQL_TSI_hour,date_inscrit,current_timestamp)} > 6;
+
 
 
