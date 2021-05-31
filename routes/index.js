a0cc2a50dcc1228b8acc467b90bf3e4145c79a74
djadweb <djadweb@gmail.com>
2021-05-31 18:16:18 +0100

    module page

--- routes/index.js
+++ routes/index.js
@@ -43,6 +43,10 @@ router.get('/registration', (req, res) => {
         res.render('registration', { layout: 'registration' });
 });
 
+router.get('/module', (req, res) => {
+    res.render('module');
+});
+
 router.get('/module_folder', (req, res) => {
     res.render('module_folder');
 });
